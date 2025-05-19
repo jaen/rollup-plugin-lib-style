4ee6c744d5a277b67f1b3a8883cf26fbce7d8920
Tomek "jaen" Mańko <hi@jaen.me>
2025-05-19 16:40:29 +0200

    Use explicit path for Babel config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,16 @@
+import Path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { babel } from '@rollup/plugin-babel';
 
 const config = {
-  plugins: [babel({ extensions: [".js", ".ts"], babelHelpers: 'bundled' })]
+  plugins: [ 
+    babel({ 
+      extensions: [".js", ".ts"],
+      babelHelpers: 'bundled',
+      configFile: Path.resolve(Path.dirname(fileURLToPath(import.meta.url)), 'babel.config.js')
+    })
+  ]
 };
 
 export default config
